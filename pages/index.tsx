f799c524aaecb0428f1ec9c7bad4a1f63287490f
topsmart20 <oswellgrundmeier42@gmail.com>
2022-05-02 15:47:06 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+import Layout from "@layouts/index";
+import Home from "@containers/Home";
+
+const Index: FC = () => {
+  return (
+    <Layout>
+      <Home></Home>
+    </Layout>
+  );
+};
+
+export default Index;
